10ff363189209174d8f33f9dd6c7346006846278
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-03-17 12:21:05 +0400

    Task Completed

---
+++ ConsoleApp7/ConsoleApp7/Group.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Group
+    {
+        private int _studentLimit;
+        private string _checkNo;
+
+        public int StudentLimit
+        {
+            get => this._studentLimit;
+            set
+            {
+                if (value >= 0)
+                {
+                    this._studentLimit = value;
+                }
+            }
+        }
+
+        public bool CheckNo(string No)
+        {
+            bool check = false;
+
+            if (!string.IsNullOrWhiteSpace(No))
+            {
+                if (char.IsUpper(No[0]) && char.IsUpper(No[1]) && char.IsDigit(No[2]) && char.IsDigit(No[3]) && char.IsDigit(No[4]))
+                {
+                    check = true;
+                }
+
+            }
+            return check;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say daxil edin:");
+
+        }
+
+   
+    }
+}
